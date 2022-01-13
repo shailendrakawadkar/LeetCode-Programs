178924146d26c2d0ec23311dd461a2025db8639c
Shailendra Kawadkar <44092499+shailendrakawadkar@users.noreply.github.com>
2022-01-13 21:33:51 +0530

    Added better solution

--- Medium/287.-Find-the-Duplicate-Number.cpp
+++ Medium/287.-Find-the-Duplicate-Number.cpp
@@ -13,3 +13,21 @@ public:
         return 0;
     }
 };
+
+//******Better solution********//
+
+//Runtime: 365 ms, faster than 5.01% of C++ online submissions for Find the Duplicate Number.
+//Memory Usage: 83.9 MB, less than 14.60% of C++ online submissions for Find the Duplicate Number.
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        unordered_map<int, int> um;
+        for(int i = 0; i < nums.size(); ++i)
+        {
+            if(um.find(nums[i]) != um.end()) return nums[i];
+            else ++um[nums[i]];
+        }
+        return 0;
+    }
+};
