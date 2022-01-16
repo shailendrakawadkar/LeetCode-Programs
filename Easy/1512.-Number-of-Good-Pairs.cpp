416b296f00f1f65a081fc937f885cdc8c78ce4fb
Shailendra Kawadkar <44092499+shailendrakawadkar@users.noreply.github.com>
2022-01-16 13:28:52 +0530

    added better solution

--- Easy/1512.-Number-of-Good-Pairs.cpp
+++ Easy/1512.-Number-of-Good-Pairs.cpp
@@ -18,3 +18,32 @@ public:
         return count;
     }
 };
+
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    
+
+ 
+//--------------BETTER SOLUTION----------------//
+
+//Runtime: 0 ms, faster than 100.00% of C++ online submissions for Number of Good Pairs.
+//Memory Usage: 7.2 MB, less than 57.90% of C++ online submissions for Number of Good Pairs.
+
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        int count = 0;
+        unordered_map<int, int> um;
+        
+        for(int i = 0; i < nums.size(); ++i) ++um[nums[i]];
+        
+        
+        for(auto it : um)
+        {
+            int size = it.second - 1;
+            count = count + size * (size + 1)/ 2;
+        }
+        return count;
+    }
+};
+
