7d18976ca2d7cebf94e7390e6534a51481566586
Shailendra Kawadkar <44092499+shailendrakawadkar@users.noreply.github.com>
2022-01-27 10:18:12 +0530

    BRUTE FORCE APPROACH faster than

--- Easy/2006.-Count-Number-of-Pairs-With-Absolute-Difference-K.cpp
+++ Easy/2006.-Count-Number-of-Pairs-With-Absolute-Difference-K.cpp
@@ -1,5 +1,8 @@
 //BRUTE FORCE APPROACH
 
+//Runtime: 16 ms, faster than 74.27% of C++ online submissions for Count Number of Pairs With Absolute Difference K.
+//Memory Usage: 12.5 MB, less than 38.89% of C++ online submissions for Count Number of Pairs With Absolute Difference K.
+
 class Solution {
 public:
     int countKDifference(vector<int>& nums, int k) {
