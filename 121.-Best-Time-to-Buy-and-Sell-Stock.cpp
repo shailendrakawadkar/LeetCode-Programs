03a8073064cd29a21ed2188f91e6a792a9fdfa71
Shailendra Kawadkar <44092499+shailendrakawadkar@users.noreply.github.com>
2022-01-28 11:08:08 +0530

    BRUTE FORCE ADDED

---
+++ 121.-Best-Time-to-Buy-and-Sell-Stock.cpp
@@ -0,0 +1,14 @@
+// BRUTE FORCH APPROACH
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0;
+        
+        for(int i = 0; i < prices.size(); ++i) 
+            for(int j = i + 1; j < prices.size(); ++j)
+                if(prices[j] - prices[i] > profit) profit = prices[j] - prices[i];
+        
+        return profit;
+    }
+};
