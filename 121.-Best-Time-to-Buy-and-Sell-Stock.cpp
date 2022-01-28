3dc9a13e79f9c10e2d3ed1de1bd84ddcd29f276e
Shailendra Kawadkar <44092499+shailendrakawadkar@users.noreply.github.com>
2022-01-28 11:32:44 +0530

    OPTIMAL SOLUTION

--- 121.-Best-Time-to-Buy-and-Sell-Stock.cpp
+++ 121.-Best-Time-to-Buy-and-Sell-Stock.cpp
@@ -12,3 +12,24 @@ public:
         return profit;
     }
 };
+
+
+// OPTIMAL SOLUTION
+//Runtime: 108 ms, faster than 91.62% of C++ online submissions for Best Time to Buy and Sell Stock.
+//Memory Usage: 93.3 MB, less than 53.15% of C++ online submissions for Best Time to Buy and Sell Stock.
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0, min_element = INT_MAX;
+        
+        for(int i = 0; i < prices.size(); ++i) {
+            if(prices[i] < min_element) min_element = prices[i];
+            
+            if(prices[i] - min_element > profit) profit = prices[i] - min_element;
+        }
+           
+        return profit;
+    }
+};
+
