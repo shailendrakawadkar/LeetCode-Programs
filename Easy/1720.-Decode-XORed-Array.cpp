e4a2533443240d0a14925c95f6662bb2ea6389ef
Shailendra Kawadkar <44092499+shailendrakawadkar@users.noreply.github.com>
2022-01-30 19:05:54 +0530

    ADDED BETTER SOLUTION

--- Easy/1720.-Decode-XORed-Array.cpp
+++ Easy/1720.-Decode-XORed-Array.cpp
@@ -15,3 +15,23 @@ public:
         return output;
     }
 };
+
+
+//BETTER SOLUTION
+//Runtime: 28 ms, faster than 79.15% of C++ online submissions for Decode XORed Array.
+//Memory Usage: 24.7 MB, less than 99.36% of C++ online submissions for Decode XORed Array.
+
+class Solution {
+public:
+    vector<int> decode(vector<int>& encoded, int first) {
+        
+        encoded.insert(encoded.begin(), first);
+        
+        for(int i = 1; i < encoded.size(); ++i)
+        {
+            encoded[i] = encoded[i] ^ encoded[i - 1];
+        }
+        
+        return encoded;
+    }
+};
