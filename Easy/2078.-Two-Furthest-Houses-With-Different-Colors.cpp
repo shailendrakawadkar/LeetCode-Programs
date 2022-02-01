3b720b25929fb77be29aeb425f88fc1554948abf
Shailendra Kawadkar <44092499+shailendrakawadkar@users.noreply.github.com>
2022-02-01 10:02:46 +0530

    BETTER SOLUTION

--- Easy/2078.-Two-Furthest-Houses-With-Different-Colors.cpp
+++ Easy/2078.-Two-Furthest-Houses-With-Different-Colors.cpp
@@ -19,3 +19,33 @@ public:
         return maxDis;
     }
 };
+
+
+//=======================BETTER SOLUTION========================//
+
+//Runtime: 4 ms, faster than 61.18% of C++ online submissions for Two Furthest Houses With Different Colors.
+//Memory Usage: 9 MB, less than 18.07% of C++ online submissions for Two Furthest Houses With Different Colors
+
+class Solution {
+public:
+    int maxDistance(vector<int>& colors) {
+        int maxDis = 0;
+        
+        for(int i = 0, j = colors.size() - 1; i < colors.size();)
+        {
+            if(i == j) {
+                ++i;
+                j = colors.size() - 1;
+                continue;
+            }
+            if(colors[i] != colors[j] && abs(i - j) > maxDis) {
+                maxDis = abs(i - j);
+                j = colors.size() - 1;
+                ++i;
+            }
+            else --j;
+        }
+        
+        return maxDis;
+    }
+};
