2168c5a1511e488165f0f2775df215907d3391f5
Shailendra Kawadkar <44092499+shailendrakawadkar@users.noreply.github.com>
2022-02-15 06:51:14 -0800

    BETTER SOLUTION

--- Easy/136.-Single-Number.cpp
+++ Easy/136.-Single-Number.cpp
@@ -19,3 +19,19 @@ public:
         return output;
     }
 };
+
+//====================BETTER SOLUTION ====================//
+
+//Runtime: 20 ms, faster than 76.93% of C++ online submissions for Single Number.
+//Memory Usage: 16.8 MB, less than 85.89% of C++ online submissions for Single Number.
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int number = nums[0];
+        
+        for(int i = 1; i < nums.size(); ++i) number ^= nums[i];
+        
+        return number;
+    }
+};
