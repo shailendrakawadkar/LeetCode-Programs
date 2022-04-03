f2a417776a7225268c9985ab5b48c6e3d4a1038b
Shailendra Kawadkar <44092499+shailendrakawadkar@users.noreply.github.com>
2022-04-03 09:49:42 +0530

    Time: 4 ms (50.00%), Space: 6 MB (66.67%) - LeetHub

--- 2224-minimum-number-of-operations-to-convert-time/2224-minimum-number-of-operations-to-convert-time.cpp
+++ 2224-minimum-number-of-operations-to-convert-time/2224-minimum-number-of-operations-to-convert-time.cpp
@@ -3,26 +3,18 @@ public:
     int convertTime(string current, string correct) {
         int need_min = (stoi(correct.substr(0,2)) - stoi(current.substr(0,2))) * 60 + (stoi(correct.substr(3)) - stoi(current.substr(3)));
         int ct = 0;
-        while(need_min >= 60)
-        {
-            need_min -= 60;
-            ++ct;
-        }
-        while(need_min >= 15)
-        {
-            need_min -= 15;
-            ++ct;
-        }
-        while(need_min >= 5)
-        {
-            need_min -= 5;
-            ++ct;
-        }
-        while(need_min >= 1)
-        {
-            need_min -= 1;
-            ++ct;
-        }
+        ct += need_min/60;
+        need_min -= (60 * (need_min/60));
+        
+        ct += need_min/15;
+        need_min -= (15 * (need_min/15));
+        
+        ct += need_min/5;
+        need_min -= (5 * (need_min/5));
+        
+        ct += need_min/1;
+        need_min -= (1 * (need_min/1));
+        
         return ct;
     }
 };
