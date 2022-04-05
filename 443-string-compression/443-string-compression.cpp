bdcf6c24b1a55206ef3f4e5443b19247a642c1f2
Shailendra Kawadkar <44092499+shailendrakawadkar@users.noreply.github.com>
2022-04-05 20:30:38 +0530

    Time: 6 ms (63.85%), Space: 9.2 MB (5.60%) - LeetHub

--- 443-string-compression/443-string-compression.cpp
+++ 443-string-compression/443-string-compression.cpp
@@ -1,30 +1,34 @@
 class Solution {
 public:
+    void insert_char(vector<char>& output, char ch, int ct)
+    {
+        output.push_back(ch);
+        if(ct > 1)
+        {
+            string str = to_string(ct);
+            for(int j = 0; j < str.size(); ++j) output.push_back(str[j]);
+        }
+    }
     int compress(vector<char>& chars) {
         vector<char> output;
         int ct = 1;
-        for(int i = 1; i < chars.size(); ++i)
+        for(int i = 1; i < chars.size() + 1; ++i)
         {
+            if(i == chars.size())
+            {
+                insert_char(output, chars.back(), ct);                
+            }
+            else
+            {
             if(chars[i] != chars[i - 1])
             {
-                output.push_back(chars[i - 1]);
-                if(ct > 1)
-                {
-                    string str = to_string(ct);
-                    for(int j = 0; j < str.size(); ++j) output.push_back(str[j]);
-                    ct = 1;
-                }
+                insert_char(output, chars[i - 1], ct);
+                ct = 1;
             }
             else ++ct;
+                 
+            }
         }
-        
-        output.push_back(chars.back());
-        if(ct > 1)
-        {
-             string str = to_string(ct);
-                    for(int j = 0; j < str.size(); ++j) output.push_back(str[j]);
-                    ct = 1;
-                }
         
         chars.clear();
         chars = output;
