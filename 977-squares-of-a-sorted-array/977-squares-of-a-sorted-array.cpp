8f06d22ffd7670a6a89c4f325cae9d7d29a79b32
Shailendra Kawadkar <44092499+shailendrakawadkar@users.noreply.github.com>
2022-06-10 02:57:59 -0700

    Time: 55 ms (33.54%), Space: 25.9 MB (82.28%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        vector<int> output;
-        int i = 0, j = nums.size() - 1;
+        vector<int> output(nums.size());
+        int i = 0, j = nums.size() - 1, k = nums.size() - 1;
         
         while(i <= j)
         {
             if(abs(nums[i]) > abs(nums[j]))
             {
-                output.insert(output.begin(), abs(nums[i]) * abs(nums[i]));
+                output[k] = abs(nums[i]) * abs(nums[i]);
+                k--;
                 ++i;
             }else
             {
-                output.insert(output.begin(), abs(nums[j]) * abs(nums[j]));
+                output[k] = abs(nums[j]) * abs(nums[j]);
                 --j;
+                k--;
             }
         }
         return output;
